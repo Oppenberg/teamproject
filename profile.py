08daf1f186002bf9bd74828e9ee4f6cae03f44dd
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-07 11:11:36 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -26,8 +26,8 @@ for i in range(3):
     node = request.XenVM("head")
   else:
     node = request.XenVM("worker-" + str(i))
-  node.cores = 4
-  node.ram = 8192
+  node.cores = 12
+  node.ram = 32000
   node.routable_control_ip = "true" 
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
