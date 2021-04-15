a72a639493b30490348e78feb3a08baeb0ceeec4
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-15 19:34:21 -0400

    chmod benchmark and installs numactl

---
+++ install_enableUtil.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt install numactl -y
+
+chmod +x /local/repository/benchmarks/runme_xeon64
+chmod +x /local/repository/benchmarks/lininput_xeon64
+chmod +x /local/repository/benchmarks/xlinpack_xeon64
