447458b4ef2337a28c22fa590e2766fc159f3ccc
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-15 20:45:52 -0400

    Update install_enableUtil.sh

--- install_enableUtil.sh
+++ install_enableUtil.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 sudo apt install numactl -y
 
-sudo bash chmod +x /local/repository/benchmarks/runme_xeon64
-sudo bash chmod +x /local/repository/benchmarks/lininput_xeon64
-sudo bash chmod +x /local/repository/benchmarks/xlinpack_xeon64
+chmod +x /local/repository/benchmarks/runme_xeon64
+chmod +x /local/repository/benchmarks/lininput_xeon64
+chmod +x /local/repository/benchmarks/xlinpack_xeon64
 
 mv /local/repository/* /users/Am943807/linpack
