7470f8eda38cbe4df6fb5334a9aedcfd7b439a43
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-19 01:04:31 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,39 +1,24 @@
- 
+"""An example of constructing a profile with install and execute services. 
+Instructions:
+Wait for the profile instance to start, then click on the node in the topology
+and choose the `shell` menu item. The install and execute services are handled
+automatically during profile instantiation, with no manual intervention required.
+"""
+
+# Import the Portal object.
 import geni.portal as portal
-import geni.rspec.pg as pg
-import geni.rspec.igext as IG
+# Import the ProtoGENI library.
+import geni.rspec.pg as rspec
 
-pc = portal.Context()
-request = pc.makeRequestRSpec()
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.RawPC("node")
 
-tourDescription = \
-"""
-This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
-"""
+# Request that a specific image be installed on this node
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD";
 
-#
-# Setup the Tour info with the above description and instructions.
-#  
-tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
-request.addTour(tour)
-
-prefixForIP = "192.168.1."
-link = request.LAN("lan")
-
-for i in range(3):
-  if i == 0:
-    node = request.XenVM("head")
-  else:
-    node = request.XenVM("worker-" + str(i))
-  node.cores = 4
-  node.ram = 8192
-  node.routable_control_ip = "true" 
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
-  
-pc.printRequestRSpec(request)
+# Install and execute scripts on the node. 
+node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh")) # Installs docker
+portal.context.printRequestRSpec()
