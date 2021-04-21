9cb60f2748d249481f771e5eeba7d152f05f1b38
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-21 17:37:03 -0400

    Create runLocal.sh

---
+++ runLocal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#running local
+cd bin
+
+time numactl --physcpubind=0-7,16-23 --localalloc ./stream.exe
+echo "--------------------------------------------------------------------------------"
+time numactl --physcpubind=0-31 --interleave=0,1 ./stream.exe
