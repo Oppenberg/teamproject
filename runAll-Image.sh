8983e93e7c0611803daafc2e6cdb712cc891269c
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-28 17:47:05 -0400

    Create runAll-Image.sh

---
+++ runAll-Image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker pull mintyblot/stream-1:latest
+ocker run -ti --privileged mintyblot/stream-1:latest
+docker image rm -f mintyblot/stream-1:latest
+
+docker pull mintyblot/stream-2:latest
+ocker run -ti --privileged mintyblot/stream-2:latest
+docker image rm -f mintyblot/stream-2:latest
+
+cd bin
+time numactl --physcpubind=0-7,16-23 --localalloc ./stream.exe
+time numactl --physcpubind=0-31 --interleave=0,1 ./stream.exe
