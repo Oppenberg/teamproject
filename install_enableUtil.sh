c28a48e8f7555beea74b9b787a7e7c8c0bf12e5d
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-28 17:42:14 -0400

    Update install_enableUtil.sh

--- install_enableUtil.sh
+++ install_enableUtil.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+echo "Enter user dir to build to:"
+read varname
+
 sudo apt-get update
 sudo apt install numactl -y
 
-make $all
 chmod +x /local/repository/runLocal.sh
 chmod +x /local/repository/runDocker.sh
 chmod +x /local/repository/runAll.sh
 
+mkdir -p /users/$varname/stream
 
-mv /local/repository/* /users/Am943807/stream
+mv /local/repository/* /users/$varname/stream
+
+make $all
