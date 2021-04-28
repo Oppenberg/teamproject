e5f989116fdb745e5e3c3b9659350a9b5ec57258
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-28 18:51:24 -0400

    Update runAllImage.sh

--- runAllImage.sh
+++ runAllImage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 docker pull mintyblot/stream-1:latest
-docker run -ti --privileged mintyblot/stream-1:latest
+for ((i=0;i<10;i++)); do docker run -ti --privileged mintyblot/stream-1:latest; done
 docker image rm -f mintyblot/stream-1:latest
 
 docker pull mintyblot/stream-2:latest
-docker run -ti --privileged mintyblot/stream-2:latest
+for ((i=0;i<10;i++)); do docker run -ti --privileged mintyblot/stream-2:latest; done
 docker image rm -f mintyblot/stream-2:latest
 
 cd bin
-time numactl --physcpubind=0-7,16-23 --localalloc ./stream.exe
-time numactl --physcpubind=0-31 --interleave=0,1 ./stream.exe
+for ((i=0;i<10;i++)); do time numactl --physcpubind=0-7,16-23 --localalloc ./stream.exe; done
+for ((i=0;i<10;i++)); do time numactl --physcpubind=0-31 --interleave=0,1 ./stream.exe; done
