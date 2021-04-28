dd4a35b1afaf8302bc4c3913da3cfe6664497d2d
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-04-28 18:03:42 -0400

    Update install_enableUtil.sh

--- install_enableUtil.sh
+++ install_enableUtil.sh
@@ -9,11 +9,11 @@ sudo apt install numactl -y
 chmod +x /local/repository/runLocal.sh
 chmod +x /local/repository/runDocker.sh
 chmod +x /local/repository/runAll.sh
-chmod +x /local/repository/runAll-Images.sh
-
+chmod +x /local/repository/runAll-Image.sh
+make $all
 
 mkdir -p /users/$varname/stream
 
 mv /local/repository/* /users/$varname/stream
 
-make $all
+
