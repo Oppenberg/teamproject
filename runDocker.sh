3875396c8b271250344bf945bb0030f8b03eaa6b
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-05-02 19:54:22 -0400

    Update runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -7,25 +7,25 @@ if [ "$#" -ne 1 ]; then
 fi
 
 if [ "$1" -eq 1 ]; then
-    rm -f Dockerfile
-    ln -s Dockerfile.OneSocket Dockerfile
+    sudo rm -f Dockerfile
+    sudo ln -s Dockerfile.OneSocket Dockerfile
 elif [ "$1" -eq 2 ]; then
-    rm -f Dockerfile
-    ln -s Dockerfile.TwoSocket Dockerfile
+    sudo rm -f Dockerfile
+    sudo ln -s Dockerfile.TwoSocket Dockerfile
 else
     echo "Usage: $0 numberOfSockets (specify as 1 or 2)"
     exit 1
 fi
 
-docker build -t stream .
+sudo docker build -t stream .
 
 
 now=`date`
 echo "Running Stream, started at $now"
 echo "--------------------------------------------------------------------------------"
 echo "Running Stream, started at $now"
-docker run -ti --privileged stream
-docker image rm -f stream
-rm -f Dockerfile
+sudo docker run -ti --privileged stream
+sudo docker image rm -f stream
+sudo rm -f Dockerfile
 echo ""
 echo -n "Experiment completed at "; date
