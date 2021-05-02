e16e0313f2f19fe0f66a2a6bd2b9359f12a49a1e
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-05-02 19:48:49 -0400

    Update runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -7,25 +7,25 @@ if [ "$#" -ne 1 ]; then
 fi
 
 if [ "$1" -eq 1 ]; then
-    rm -f Dockerfile
-    ln -s Dockerfile.OneSocket Dockerfile
+    sudo rm -f Dockerfile
+    sudo ln -s Dockerfile.OneSocket Dockerfile
 elif [ "$1" -eq 2 ]; then
-    rm -f Dockerfile
-    ln -s Dockerfile.TwoSocket Dockerfile
+    sudo rm -f Dockerfile
+    sudo ln -s Dockerfile.TwoSocket Dockerfile
 else
     echo "Usage: $0 numberOfSockets (specify as 1 or 2)"
     exit 1
 fi
 
-docker build -t RandomAccess .
+sudo docker build -t randomaccess .
 
 
 now=`date`
 echo "Running Random Access, started at $now"
 echo "--------------------------------------------------------------------------------"
 echo "Running Random access, started at $now"
-docker run -ti --privileged RandomAccess
-docker image rm -f RandomAccess
-rm -f Dockerfile
+sudo docker run -ti --privileged randomaccess
+sudo docker image rm -f randomaccess
+sudo rm -f Dockerfile
 echo ""
 echo -n "Experiment completed at "; date
