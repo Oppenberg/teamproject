870fafca85a29b44cb67d425b7f9cd5b9e27ea09
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2021-05-02 19:26:10 -0400

    Update runAllImage.sh

--- runAllImage.sh
+++ runAllImage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker pull mintyblot/randomaccess1:latest
-for ((i=0;i<10;i++)); do docker run -ti --privileged mintyblot/randomaccess1:latest; done
-docker image rm -f mintyblot/randomaccess1:latest
+sudo docker pull mintyblot/randomaccess1:latest
+for ((i=0;i<10;i++)); do sudo docker run -ti --privileged mintyblot/randomaccess1:latest; done
+sudo docker image rm -f mintyblot/randomaccess1:latest
 
-docker pull mintyblot/randomaccess2:latest
-for ((i=0;i<10;i++)); do docker run -ti --privileged mintyblot/randomaccess2:latest; done
-docker image rm -f mintyblot/randomaccess2:latest
+sudo docker pull mintyblot/randomaccess2:latest
+for ((i=0;i<10;i++)); do sudo docker run -ti --privileged mintyblot/randomaccess2:latest; done
+sudo docker image rm -f mintyblot/randomaccess2:latest
 
 cd bin
 for ((i=0;i<10;i++)); do time numactl --physcpubind=0-7,16-23 --localalloc ./gups.exe; done
